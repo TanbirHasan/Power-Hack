aa311dec2dcecb4db36473a52d3907d622a8b064
unknown <mdtanbir576@gmail.com>
2022-06-30 12:26:14 +0600

    header

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Header from './components/Header';
+import BillingPage from './components/BillingPage';
+import { createContext, useState } from 'react';
+
+
+export const SetUser = createContext()
+export const User = createContext()
+
+
+function App() {
+
+  const [user ,setUser] = useState([])
+  return (
+    <div className="App">
+      <h3>User name : {user?.fullname}</h3>
+      <SetUser.Provider value={setUser}>
+        <User.Provider value={user}>
+          <Header />
+          <BillingPage />
+        </User.Provider>
+      </SetUser.Provider>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+      <div className='bg-orange-200'>
+        <div className='flex justify-between px-10'>
+            <h2>Logo</h2>
+            <h3>Paid Total : 0</h3>
+        </div>
+      
+      </div>
+    );
+};
+
+export default Header;
